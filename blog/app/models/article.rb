72bb0bbc17c288ad12fbef81da43f5ee96be59a0
Rinat Khanov <rinat@khanov.com>
2015-12-01 22:08:02 +0300

    add more styling to forms

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+  validates :text, presence: true,
+                    length: { minimum: 10 }
 end
--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  validates :commenter, presence: true,
+                    length: { minimum: 3 }
+  validates :body, presence: true
 end
