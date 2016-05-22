4ceaf07ad08b8000b3737ae8f2c77ea5c9c861a7
Rinat Khanov <rinat@khanov.com>
2016-05-22 21:51:11 +0300

    Remove user email verification

--- CMS/app/models/user.rb
+++ CMS/app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
   has_many :pages
 
